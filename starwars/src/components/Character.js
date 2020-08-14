1e9e0d5a854bb0090339ed4a8dcebd6ab37240d6
Melissa C <66797764+Wednesdayw@users.noreply.github.com>
2020-08-14 13:27:44 -0400

    updates to App.js and Character.js

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React from 'react';
+import styled from "styled-components"
+import { Container } from '../App'
+
+const P = styled.p`
+    ${props =>
+        props.type === 'name' &&
+       `font-size: 20px;
+        color: blue;
+        `
+    }
+
+    ${props =>
+        props.type === 'location' &&
+       `font-size: 16px;
+        color: lime; 
+        `
+    }
+`;
+
+const Character = (props) => {
+    return (
+            <Container>
+                <img src={props.image} alt='Morty watch out for the aliens'/>
+                <P type='name'>{props.name}</P>
+                <P type='location'>Originally from: {props.location}</P>
+            </Container>
+        
+    )
+}
+
+export default Character;
